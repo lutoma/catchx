0c38728a1ded76b2bc2bc009bc858b7ce0ca3ff4
Lukas Martini <lukas.martini@junge-piraten.de>
2010-06-14 11:55:23 +0200

    Fixed serverside bug

--- modules/connector.py
+++ modules/connector.py
@@ -35,7 +35,7 @@ class connector(dict):
 
 		# –––– S→C Commands ––––
 	def scc_started(self, par):
-		self.ui.chat_update(_("* {0} started the game").format(par))
+		self.ui.chat_update(_("* {0} started the game").format(par[0]))
 		self.started = True
 
 	def scc_chat(self, par):
@@ -49,10 +49,10 @@ class connector(dict):
 		self.ui.map.map_layout.put(self.ui.map.figure, int(round(par[1] - 16,0)), int(round(par[2] - 45, 0)))
 				
 	def scc_joined(self, par):
-		self.ui.chat_update(_("* {0} entered the room").format(par))
+		self.ui.chat_update(_("* {0} entered the room").format(par[0]))
 		
 	def scc_left(self, par):
-		self.ui.chat_update(_("* {0} left the room").format(par))
+		self.ui.chat_update(_("* {0} left the room").format(par[0]))
 		
 	def scc_color_assoc(self, par):
 		self.color_assoc = par
@@ -78,11 +78,11 @@ class connector(dict):
 		while self.session:
 			cmd, par = self.server.poll_message(self.session)
 
+			if len(par) > 0 and not isinstance(par[0], str):
+				par = par[0]
+
 			if not cmd == None:
 				print "{0}Broadcast: {1}{2}{3}".format(green,cmd,par,normal)
-
-			if len(par) > 0:
-				par = par[0]
 
 			if cmd in self.commands:
 				self.commands[cmd](self, par)
