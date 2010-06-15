1e9eb077bb2bf2e99eba3267ec1dd67dc82968a8
Lukas Martini <lukas.martini@junge-piraten.de>
2010-06-15 18:25:11 +0200

    Works with catchx version we got now

--- tools/devel.sh
+++ tools/devel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd `dirname $0`/..
 ./server.py -p 20211 2> /dev/null &
-./start.py -lcr test -s localhost -p 20211 -k test -u CatchX\ Developer
+#./start.py -lcr test -s localhost -p 20211 -k test -u CatchX\ Developer
+./start.py -s localhost -p 20211 -n CatchX\ Developer
 kill %%
